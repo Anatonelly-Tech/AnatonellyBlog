cf17c0da025c9ad504cde99dafc110182b427d9e
Vitor Dallanol <125999178+Crautor@users.noreply.github.com>
2024-04-29 13:15:41 -0300

    feat: add scrollTrigger (now the animation play if the viewport stay on trigger)

--- src/components/AnatonellyTech/index.jsx
+++ src/components/AnatonellyTech/index.jsx
@@ -7,58 +7,124 @@ import '@/components/AnatonellyTech/style.css';
 import gsap, { toArray } from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { Flip } from 'gsap/all';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const index = () => {
   gsap.registerPlugin(useGSAP);
+  gsap.registerPlugin(ScrollTrigger);
   gsap.registerPlugin(Flip);
+  var tl = 0;
+  var curLayout = 0;
 
-  const container = useRef();
+  const introduction = useRef();
+  const Tech = useRef();
   useGSAP(
     () => {
-      let layouts = [];
+      gsap.to('#introduction', {
+        scrollTrigger: {
+          trigger: '#introduction',
+          onEnter: () => {
+            tl = 1;
+            let layouts = [];
 
-      window.innerWidth <= 980
-        ? (layouts = ['final', 'plain', 'none'])
-        : (layouts = ['final', 'plain', 'columns', 'none']);
+            window.innerWidth <= 980
+              ? (layouts = ['final', 'plain', 'none'])
+              : (layouts = ['final', 'plain', 'columns', 'none']);
 
-      let curLayout = 0;
-      function nextState() {
-        const classList = ['.letter', '.for', '.anatonelly'];
-        const state = Flip.getState(classList, {
-          props: 'color,backgroundColor',
-          simple: true,
-        });
+            function nextState() {
+              const classList = ['.letter', '.for', '.anatonelly'];
+              const state = Flip.getState(classList, {
+                props: 'color,backgroundColor',
+                simple: true,
+              });
 
-        container.current.classList.remove(layouts[curLayout]);
-        curLayout = (curLayout + 1) % layouts.length;
-        container.current.classList.add(layouts[curLayout]);
+              introduction.current.classList.remove(layouts[curLayout]);
+              curLayout = (curLayout + 1) % layouts.length;
+              introduction.current.classList.add(layouts[curLayout]);
+              Flip.from(state, {
+                absolute: true,
+                stagger: 0.07,
+                duration: 0.5,
+                ease: 'power2.inOut',
+                spin: curLayout == 0,
+                simple: true,
+                onEnter: (elements, animation) =>
+                  gsap.fromTo(
+                    elements,
+                    { opacity: 0 },
+                    { opacity: 1, delay: animation.duration() - 0.1 }
+                  ),
+                onLeave: (elements) => gsap.to(elements, { opacity: 0 }),
+              });
+              tl === 1
+                ? gsap.delayedCall(curLayout === 0 ? 3.5 : 1.5, nextState)
+                : null;
+            }
+            gsap.delayedCall(1, nextState);
+          },
+          onLeave: () => {
+            tl = 0;
+          },
+          onEnterBack: () => {
+            tl = 1;
+            let layouts = [];
 
-        Flip.from(state, {
-          absolute: true,
-          stagger: 0.07,
-          duration: 0.5,
-          ease: 'power2.inOut',
-          spin: curLayout == 0,
-          simple: true,
-          onEnter: (elements, animation) =>
-            gsap.fromTo(
-              elements,
-              { opacity: 0 },
-              { opacity: 1, delay: animation.duration() - 0.1 }
-            ),
-          onLeave: (elements) => gsap.to(elements, { opacity: 0 }),
-        });
+            window.innerWidth <= 980
+              ? (layouts = ['final', 'plain', 'none'])
+              : (layouts = ['final', 'plain', 'columns', 'none']);
 
-        gsap.delayedCall(curLayout === 0 ? 3.5 : 1.5, nextState);
-      }
-      gsap.delayedCall(1, nextState);
+            function nextState() {
+              const classList = ['.letter', '.for', '.anatonelly'];
+              const state = Flip.getState(classList, {
+                props: 'color,backgroundColor',
+                simple: true,
+              });
+
+              introduction.current.classList.remove(layouts[curLayout]);
+              curLayout = (curLayout + 1) % layouts.length;
+              introduction.current.classList.add(layouts[curLayout]);
+              Flip.from(state, {
+                absolute: true,
+                stagger: 0.07,
+                duration: 0.5,
+                ease: 'power2.inOut',
+                spin: curLayout == 0,
+                simple: true,
+                onEnter: (elements, animation) =>
+                  gsap.fromTo(
+                    elements,
+                    { opacity: 0 },
+                    { opacity: 1, delay: animation.duration() - 0.1 }
+                  ),
+                onLeave: (elements) => gsap.to(elements, { opacity: 0 }),
+              });
+              tl === 1
+                ? gsap.delayedCall(curLayout === 0 ? 3.5 : 1.5, nextState)
+                : null;
+            }
+            gsap.delayedCall(1, nextState);
+          },
+          onLeaveBack: () => {
+            tl = 0;
+          },
+          toggleActions: 'play pause play pause',
+          start: 'top center',
+          end: 'bottom center',
+          scrub: 0.5,
+        },
+      });
     },
-    { scope: container }
+    { scope: Tech }
   );
+
   return (
-    <div id='AnatonellyTech' className='w-full h-auto flex flex-col '>
+    <div
+      id='AnatonellyTech'
+      ref={Tech}
+      className='w-full h-auto flex flex-col '
+    >
       <div className='bg-neutral-800 h-screen w-full flex flex-col'>
-        <div ref={container} id='container' className='final'>
+        <div ref={introduction} id='introduction' className='final'>
           <div className='letter T'>T</div>
           <div className='letter e'>e</div>
           <div className='letter c'>c</div>
