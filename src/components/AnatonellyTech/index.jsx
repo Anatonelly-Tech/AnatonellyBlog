d2e2326ccc3f86b716b32f90aa3b1198a82bf531
Vitor Dallanol <125999178+Crautor@users.noreply.github.com>
2024-04-30 13:14:20 -0300

    feat: add some informations about Anatonelly Tech

--- src/components/AnatonellyTech/index.jsx
+++ src/components/AnatonellyTech/index.jsx
@@ -4,20 +4,36 @@ import React, { useRef } from 'react';
 import '@/components/AnatonellyTech/style.css';
 
 // Libs
-import gsap, { toArray } from 'gsap';
+import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { Flip } from 'gsap/all';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
+// Utils
+import { TitleLetter, Paragraph, Lists, Img } from '@/utils/animations';
+
+// Components
+import CardTech from '@/components/CardTech';
+
+// Icons;
+import { LuMonitor } from 'react-icons/lu';
+import { FaGears, FaPeopleCarryBox } from 'react-icons/fa6';
+import { IoTerminal } from 'react-icons/io5';
+
 const index = () => {
-  gsap.registerPlugin(useGSAP);
-  gsap.registerPlugin(ScrollTrigger);
-  gsap.registerPlugin(Flip);
+  // Plugins
+  gsap.registerPlugin(useGSAP, ScrollTrigger, Flip);
+
+  // Variables important for the animation
+  // tl is a toggle for the animation if iquals 1 the animation is running
+  // curLayout is the current layout of the animation
   var tl = 0;
   var curLayout = 0;
 
+  // Refs
   const introduction = useRef();
   const Tech = useRef();
+
   useGSAP(
     () => {
       gsap.to('#introduction', {
@@ -113,6 +129,37 @@ const index = () => {
           scrub: 0.5,
         },
       });
+
+      // text reveal
+      const scrub = 0.5;
+      gsap.set('.titleReveal > span', {
+        '--progress': 0,
+        backgroundPositionX:
+          'calc(-200vmax + (var(--progress) * 200vmax)), calc(-200vmax + (var(--progress) * 200vmax)), 0',
+        color: 'transparent',
+        markers: true,
+      });
+
+      gsap.to('.titleReveal > span', {
+        '--progress': 1,
+        scrollTrigger: {
+          markers: true,
+          trigger: '#titleReveal',
+          scrub,
+          start: 'top center',
+          end: 'center-=10% center-=10%',
+        },
+      });
+
+      async function loadReveal() {
+        const sr = (await import('scrollreveal')).default();
+
+        sr.reveal('#TitleLetter', TitleLetter);
+        sr.reveal('#Paragraph', Paragraph);
+        sr.reveal('#Lists', Lists);
+        sr.reveal('#Img', Img);
+      }
+      loadReveal();
     },
     { scope: Tech }
   );
@@ -131,6 +178,73 @@ const index = () => {
           <div className='letter h'>h</div>
           <div className='for'>for</div>
           <div className='anatonelly'>Anatonelly</div>
+        </div>
+      </div>
+      <div className='h-auto w-full flex flex-col'>
+        <div
+          id='titleReveal'
+          className='w-full h-auto p-10 flex items-start flex-col justify-center bg-neutral-800'
+        >
+          <p className='titleReveal font-black  '>
+            <span className=''>Seja Bem Vindo!</span>
+          </p>
+          <div className='h-auto w-full flex flex-col items-center p-10 pt-0 justify-center gap-10 bg-neutral-800'>
+            <img
+              id='Img'
+              className='w-80 shadow-neutral-300'
+              src='/InicialScreen/AnatonellyTech.svg'
+              alt=''
+            />
+            <p
+              id='TitleLetter'
+              className='font-black md:text-xl xxs:text- text-center text-white '
+            >
+              Na Anatonnely Tech, estamos aqui para tornar sua visão digital uma
+              realidade. Somos uma equipe apaixonada por tecnologia,
+              especializada em criar sites personalizados e oferecer serviços
+              integrados para a área de logística. Seja qual for a sua
+              necessidade digital, estamos aqui para superar suas expectativas.
+            </p>
+          </div>
+        </div>
+        <div className='flex mb-10 flex-col items-center md:p-10 xxs:p-5 justify-center gap-5'>
+          <div className='flex xxs:flex-col gap-5 md:flex-row w-full '>
+            <CardTech
+              icon={<LuMonitor size={30} className='invert' />}
+              title={'Criação de Sites Personalizados'}
+              topics={[
+                'Desenvolvimento de sites com design moderno e funcionalidades avançadas.',
+                'Foco na experiência do usuário para destacar o negócio em um mercado digital competitivo.',
+              ]}
+            />
+            <CardTech
+              icon={<FaGears size={30} className='invert' />}
+              title={'Soluções Integradas para Logística'}
+              topics={[
+                'Soluções personalizadas para otimizar operações logísticas.',
+                'Inclui rastreamento de cargas, gestão de estoque e outras necessidades específicas da indústria.',
+              ]}
+            />
+          </div>
+          <div className='flex xxs:flex-col gap-5 md:flex-row w-full'>
+            <CardTech
+              icon={<IoTerminal size={30} className='invert' />}
+              title={'Software Sob Medida'}
+              topics={[
+                'Desenvolvimento de software personalizado para atender às necessidades únicas de cada empresa.',
+                'Aplicações internas para aumentar a eficiência e sistemas completos de gerenciamento de processos são exemplos.',
+              ]}
+            />
+
+            <CardTech
+              icon={<FaPeopleCarryBox size={30} className='invert' />}
+              title={'Parceria com o Cliente'}
+              topics={[
+                'Abordagem colaborativa e transparente centrada no cliente.',
+                'Envolvimento do cliente em todas as etapas do processo, transformando suas ideias em soluções digitais de sucesso.',
+              ]}
+            />
+          </div>
         </div>
       </div>
     </div>
